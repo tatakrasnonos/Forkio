c5b1d022d7740928fb65aac7f945668cdebc0117
Tanya Krasnonos <krasnonos.tv@gmail.com>
2020-07-12 13:39:13 +0300

    change burger

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,18 @@
-const hamburgerBtn = document.querySelector('.navbar__burger');
-const navbarList = document.querySelector('.navbar__list');
+document.addEventListener('DOMContentLoaded', (event) => {
+  const hamburgerBtn = document.querySelector('.navbar__burger');
+  const navbarList = document.querySelector('.navbar__list');
 
-hamburgerBtn.addEventListener('click', () => {
-  navbarList.classList.toggle('active');
-  hamburgerBtn.classList.toggle('active');
+  hamburgerBtn.addEventListener('mouseover', (event) => {
+    event.preventDefault();
+    navbarList.classList.add('active');
+    hamburgerBtn.classList.add('active');
+  });
+
+  hamburgerBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    if (hamburgerBtn.classList.contains('active')) {
+      navbarList.classList.remove('active');
+      hamburgerBtn.classList.remove('active');
+    }
+  });
 });
